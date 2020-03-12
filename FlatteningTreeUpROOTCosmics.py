ce297732d648ecbc70c9d1e71607f60656ff7d4b
Sophie Middleton <sophie.middleton@manchester.ac.uk>
2020-03-12 16:43:48 -0500

    Create FlatteningTreeUpROOTCosmics.py

---
+++ FlatteningTreeUpROOTCosmics.py
@@ -0,0 +1,16 @@
+import uproot
+import matplotlib.pyplot as plt
+
+
+file = uproot.open("../RootFiles/CosmicsWeirdShift.root")
+
+cosmics = file["CosmicTrackDetails"]["cosmic_tree"] # opens the 'trkana' tree in the 'TrkAnaNeg' folder
+
+df = cosmics.pandas.df(flatten=True)
+
+fig, ax = plt.subplots(1,1)
+n, bins, patches = ax.hist(df["FitDOCAs"],
+                           bins=20, 
+                           range=(0,2.5), 
+                           label="cosmics")
+fig.show()
